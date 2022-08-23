fa9cb957f3e6aed72cb3932afdf4987d62150f90
Florencia <sicref001@gmail.com>
2022-08-23 16:33:06 -0300

    update main.html

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def list(request):
-    return render(request, 'frontend/list.html')
+    return render(request, 'frontend/main.html')
